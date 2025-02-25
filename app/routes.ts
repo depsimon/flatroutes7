ef78c94747858a6827e33dc1ad4267f55ae61f6a
Simon Depelchin <dep.simon@gmail.com>
2025-02-25 12:38:04 +0100

    Update: use '+' in files

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,28 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 import { remixRoutesOptionAdapter } from "@react-router/remix-routes-option-adapter";
-import { flatRoutes } from "remix-flat-routes";
+import { flatRoutes, type RouteManifest } from "remix-flat-routes";
+/**
+ * Remove Vercel-reserved characters from the routes
+ * https://github.com/kiliman/remix-flat-routes/issues/152
+ */
+function vercelRoutes(routes: RouteManifest) {
+  const fixedRoutes: RouteManifest = {};
+
+  Object.entries(routes).forEach(([routeId, route]) => {
+    route.id = routeId.replaceAll("+", "");
+
+    if (route.parentId) {
+      route.parentId = route.parentId.replaceAll("+", "");
+    }
+
+    fixedRoutes[route.id] = route;
+  });
+
+  return fixedRoutes;
+}
 
 export default remixRoutesOptionAdapter((defineRoutes) => {
-    return flatRoutes("routes", defineRoutes, {
+    const routes = flatRoutes("routes", defineRoutes, {
       ignoredRouteFiles: ['**/.*'], // Ignore dot files (like .DS_Store)
       //appDir: 'app',
       //routeDir: 'routes',
@@ -12,4 +31,6 @@ export default remixRoutesOptionAdapter((defineRoutes) => {
       nestedDirectoryChar: '-',
       //routeRegex: /((\${nestedDirectoryChar}[\/\\][^\/\\:?*]+)|[\/\\]((index|route|layout|page)|(_[^\/\\:?*]+)|([^\/\\:?*]+\.route)))\.(ts|tsx|js|jsx|md|mdx)$$/,
     });
+
+    return vercelRoutes(routes)
   });
